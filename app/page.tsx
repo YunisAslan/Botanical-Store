5d9857c0b56dd61895dcfd592c9d53187ee0efa0
YunisAslan <yunisaslanov397@gmail.com>
2023-06-30 00:20:57 +0400

    Why choose us section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import ChooseUs from "@/components/ChooseUs";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <section className="home">
       <Hero />
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt nam autem dolor repellendus expedita eum dolore dicta provident illum ut, enim exercitationem ad in unde, tenetur et accusantium illo ipsam.
-      lorem500
+      <ChooseUs />
     </section>
   );
 }
--- components/Icons.tsx
+++ components/Icons.tsx
@@ -2,9 +2,12 @@ import {
   ArrowRight,
   Carrot,
   Heart,
+  LeafIcon,
   MenuIcon,
   Search,
   ShoppingCart,
+  SproutIcon,
+  TreePineIcon,
   X,
 } from "lucide-react";
 
@@ -16,4 +19,22 @@ export const Icons = {
   menu: MenuIcon,
   heart: Heart,
   arrowRight: ArrowRight,
+  leaf: LeafIcon,
+  sprout: SproutIcon,
+  tree: TreePineIcon,
 };
+
+{
+  /* <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="#406e3f"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="lucide lucide-tree-pine"
+> */
+}
